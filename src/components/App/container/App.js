37d1c116fb2e0bc8bcf2e73010341b7597ea6dcd
Sareh Almaaz <sareh.almaaz@comjoo.com>
2019-05-05 00:21:28 +0200

    add node-sass and create app scss folder

--- src/components/App/container/App.js
+++ src/components/App/container/App.js
@@ -1,11 +1,34 @@
 import React from "react";
-import { Row, Button } from "antd";
+import { Row, Col, Card, Icon, Avatar } from "antd";
+import ReactLogo from "../../../Assets/images/react.png";
+import MobxSvg from "../../../Assets/images/mobx.svg";
+import antdLogo from "../../../Assets/images/antd.png";
+
+import "../stylesheets/App.scss";
+
+const { Meta } = Card;
 
 function App() {
   return (
-    <Row className="App">
-      antd mobx react starter
-      <Button type="danger" icon="user" />
+    <Row type="flex" justify="center" align="middle" className="app-container">
+      <Col span={24} className="card-container">
+        <Card
+          style={{ width: 600 }}
+          cover={<img alt="example" src={ReactLogo} />}
+          actions={[
+            <Icon type="setting" />,
+            <Icon type="edit" />,
+            <Icon type="ellipsis" />
+          ]}
+          extra={<Avatar src={MobxSvg} size="large" />}
+        >
+          <Meta
+            avatar={<Avatar src={antdLogo} size="large" />}
+            title="React Mobx Antd starter App"
+            description="eject CRA and modify webpack congig to add support Mobx and Antd"
+          />
+        </Card>
+      </Col>
     </Row>
   );
 }
